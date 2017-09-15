996b4dee52b2e5cf5c3963028de8c93008a8d6b2
y4izus <yaiza.garcia.mm@gmail.com>
2017-09-15 10:56:37 +0200

    Iteration #7: Pending Pickups DONE

--- starter-code/routes/laundry.js
+++ starter-code/routes/laundry.js
@@ -8,7 +8,26 @@ router.use((req, res, next) => {
 })
 
 router.get('/dashboard', (req, res, next) => {
-  res.render('laundry/dashboard')
+  let query
+
+  if (req.session.currentUser.isLaunderer) {
+    query = { launderer: req.session.currentUser._id }
+  } else {
+    query = { user: req.session.currentUser._id }
+  }
+
+  LaundryPickup
+    .find(query)
+    .populate('user', 'name')
+    .populate('launderer', 'name')
+    .sort('pickupDate')
+    .exec((err, pickupDocs) => {
+      if (err) return next(err)
+
+      res.render('laundry/dashboard', {
+        pickups: pickupDocs
+      })
+    })
 })
 
 router.post('/launderers', (req, res, next) => {
