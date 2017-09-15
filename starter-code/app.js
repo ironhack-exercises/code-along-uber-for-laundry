567b5c93d75aaf08fff9f8730909883c31e94054
y4izus <yaiza.garcia.mm@gmail.com>
2017-09-15 11:20:36 +0200

    Refactorized app.js to config/error-handler+db+express

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,82 +1,16 @@
-const express = require('express')
-const path = require('path')
-const favicon = require('serve-favicon')
-const logger = require('morgan')
-const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
-const expressLayouts = require('express-ejs-layouts')
-const mongoose = require('mongoose')
-const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
+const app = require('express')()
+require('./config/express')(app)
 
 const index = require('./routes/index')
 const users = require('./routes/users')
 const authRoutes = require('./routes/auth')
 const laundryRoutes = require('./routes/laundry')
 
-const app = express()
-
-mongoose.connect('mongodb://localhost/uberlaundry')
-
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-app.set('layout', 'layouts/main-layout')
-app.use(expressLayouts)
-app.locals.title = 'Uber for Laundry'
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-app.use(logger('dev'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
-
-app.use(session({
-  secret: 'never do your own laundry again',
-  resave: true,
-  saveUninitialized: true,
-  cookie: { maxAge: 60000 },
-  store: new MongoStore({
-    mongooseConnection: mongoose.connection,
-    ttl: 24 * 60 * 60 // 1 day
-  })
-}))
-
-app.use((req, res, next) => {
-  if (req.session.currentUser) {
-    res.locals.currentUserInfo = req.session.currentUser
-    res.locals.isUserLoggedIn = true
-  } else {
-    res.locals.isUserLoggedIn = false
-  }
-
-  next()
-})
-
 app.use('/', index)
 app.use('/users', users)
 app.use('/', authRoutes)
 app.use('/', laundryRoutes)
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  const err = new Error('Not Found')
-  err.status = 404
-  next(err)
-})
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-  // render the error page
-  res.status(err.status || 500)
-  res.render('error')
-})
+require('./config/error-handler')(app)
 
 module.exports = app
